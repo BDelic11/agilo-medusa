d791d7d7367855194dc70042ea83b6992ca1b13c
Bruno Delic <bdelic00@fesb.hr>
2025-10-09 20:24:50 +0200

    Added header and footer and added to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { getBaseURL } from "@lib/util/env"
+import Footer from "components/Footer"
+import Header from "components/Header"
 import { Metadata } from "next"
 import "styles/globals.css"
 
@@ -10,7 +12,9 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <body>
+        <Header />
         <main className="relative">{props.children}</main>
+        <Footer />
       </body>
     </html>
   )
